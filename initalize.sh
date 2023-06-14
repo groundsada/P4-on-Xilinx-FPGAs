679ea908f22c2b947f8723afdefdca7e50c8bd3f
Mohammad <msada@hawk.iit.edu>
2023-06-14 10:18:26 -0500

    Fix initialize.sh

--- initalize.sh
+++ initalize.sh
@@ -3,5 +3,5 @@
 chmod +x script.sh
 ./script.sh
 
-wget https://s3.amazonaws.com/tcpreplay-pcap-files/smallFlows.pcap
-wget https://s3.amazonaws.com/tcpreplay-pcap-files/bigFlows.pcap
+wget -nc https://s3.amazonaws.com/tcpreplay-pcap-files/smallFlows.pcap
+wget -nc https://s3.amazonaws.com/tcpreplay-pcap-files/bigFlows.pcap
